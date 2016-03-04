482ceb44bef0d91bc29b9f1feb8f78a54a43f168
Cesar Gomez <cesargomez89@gmail.com>
2016-03-04 17:51:26 -0600

    fix folder detection

--- install.sh
+++ install.sh
@@ -27,6 +27,8 @@ git clone https://github.com/cesargomez89/neovim-dotfiles.git "${HOME}/.vim" \
 
 cd "${HOME}/.vim" || die "Could not go into the ${HOME}/.vim"
 
+mkdir '~/.config/nvim/autoload'
+mkdir '~/.config/nvim/plugged'
 link "vim/vimrc"    ".vimrc"
 link "autoload" "~/.config/nvim/autoload"
 link "plugged"  "~/.config/nvim/plugged"
